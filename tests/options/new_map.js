1e993756db9b541c4413d5030c62ee1231cf994a
Daniel Dubs <Xeais@users.noreply.github.com>
2022-05-27 03:56:15 +0200

    Fixed some spacing flaws

--- tests/options/new_map.js
+++ tests/options/new_map.js
@@ -9,15 +9,15 @@ module.exports =
       test.open("index.html");
 
     test.click("#optionButton")
-      .setValue("#width", width)
-      .setValue("#height", height)
-      .click('input[type="submit"]')
-      .execute(function()
-      {
-        this.assert.ok(window.terrain.mapSizeX === width.value, "Mapwidth should be " + width.value + ", was " + window.terrain.mapSizeX + ".");
-        this.assert.ok(window.terrain.mapSizeY === height.value, "Mapwidth should be " + height.value + ", was " + window.terrain.mapSizeY + ".");
-        this.assert.ok(window.store.getItem("draft") === null, "No draft should be saved.");
-      });
+        .setValue("#width", width)
+        .setValue("#height", height)
+        .click('input[type="submit"]')
+        .execute(function()
+        {
+          this.assert.ok(window.terrain.mapSizeX === width.value, "Mapwidth should be " + width.value + ", was " + window.terrain.mapSizeX + ".");
+          this.assert.ok(window.terrain.mapSizeY === height.value, "Mapwidth should be " + height.value + ", was " + window.terrain.mapSizeY + ".");
+          this.assert.ok(window.store.getItem("draft") === null, "No draft should be saved.");
+        });
 
     if(external)
       return test;
