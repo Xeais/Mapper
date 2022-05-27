72be1e6f73e19dad9e7efb9a033413892b41a34a
Daniel Dubs <Xeais@users.noreply.github.com>
2022-05-27 04:02:33 +0200

    Fixed some spacing flaws

--- tests/helper.js
+++ tests/helper.js
@@ -1,21 +1,21 @@
 String.prototype.purple = function() 
 {
-	return "\\033[35m" + this + "\\033[0m";
+  return "\\033[35m" + this + "\\033[0m";
 };
 
 module.exports =
 {
   "fixFilename": function(filename)
   {
-		return filename.replace(/\s\([0-9]{1,}\)\./g, ".");
-	},
+    return filename.replace(/\s\([0-9]{1,}\)\./g, ".");
+  },
   "getFilename": function(filename) 
   {
-		filename = this.fixFilename(filename);
-		filename = filename.split(".")[0];
+    filename = this.fixFilename(filename);
+    filename = filename.split(".")[0];
     if(filename.split("_").length > 2) 
-			filename = filename.substr(0, filename.lastIndexOf("_"));
+      filename = filename.substr(0, filename.lastIndexOf("_"));
 
-		return filename;
-	}
+    return filename;
+  }
 };
