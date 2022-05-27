c20f94a7449074c6d77ade0e4203cc9a820af330
Daniel Dubs <Xeais@users.noreply.github.com>
2022-05-27 04:27:26 +0200

    Fixed quite some spacing problems and unified all quotation marks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,56 @@
-module.exports = function (grunt) {
-	"use strict";
+module.exports = function (grunt) 
+{
+  "use strict";
 
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json'),
-		jshint: {
-			all: [
-				"js/**/*.js",
-				"tests/**/*.js",
-				"*.json"
-			],
-			options: {
-				jshintrc: '.jshintrc'
-			}
-		},
-		lint5: {
-			dirPath: "",
-			templates: [
-				"index.html",
-				"./tests/convert.html"
-			],
-			ignoreList: [
-				'.mpr',
-				'.csv'
-			]
-		},
-		dalek: {
-			dist: {
-				src: [
-					'./tests/suite/defaultMap.js' /*,
-					'./tests/suite/options.js',
-					'./tests/suite/tickets.js'*/
-				]
-			}
-		}
-	});
+  grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  jshint: 
+  {
+    all: 
+    [
+      "js/**/*.js",
+      "tests/**/*.js",
+      "*.json"
+    ],
+    options:
+    {
+      jshintrc: '.jshintrc'
+    }
+  },
+  lint5: 
+  {
+    dirPath: "",
+    templates: 
+    [
+      "index.html",
+      "./tests/convert.html"
+    ],
+    ignoreList:
+    [
+      ".mpr",
+      ".csv"
+    ]
+  },
+  dalek: 
+  {
+    dist: 
+    {
+      src: 
+      [
+        "./tests/suite/defaultMap.js"/*,
+	"./tests/suite/options.js",
+	"./tests/suite/tickets.js"*/
+      ]
+    }
+  }
+});
 
-	grunt.loadNpmTasks('grunt-lint5');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-	grunt.loadNpmTasks('grunt-dalek');
+grunt.loadNpmTasks("grunt-lint5");
+grunt.loadNpmTasks("grunt-contrib-jshint");
+grunt.loadNpmTasks("grunt-dalek");
 
-	grunt.registerTask('test', ['validate', 'dalek']);
-	grunt.registerTask('validate', ['jshint'/*, 'lint5'*/]);
+grunt.registerTask("test", ["validate", "dalek"]);
+grunt.registerTask("validate", ["jshint"/*, "lint5"*/]);
 
-	grunt.registerTask('default', ['test']);
+grunt.registerTask("default", ["test"]);
 };
